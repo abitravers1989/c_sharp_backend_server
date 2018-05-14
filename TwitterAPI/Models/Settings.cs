7e79f23a4d5d3f6c78fadd7422a540c7673fbfd9
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-05-14 17:44:34 +0100

    Adding comments to Settings data transfer object

--- TwitterAPI/Models/Settings.cs
+++ TwitterAPI/Models/Settings.cs
@@ -7,3 +7,8 @@ namespace TwitterAPI.Models
         public string Database { get; set; }
     }
 }
+
+/// DTO (Data Transfer Object) ... just has data and nothing else .... way to pass data around application or 
+/// pass a structure around ..
+/// want to be able to instansiate an object of this class and give it some values .. data transfer .. transfer data around your app 
+///  plain old .. object or class that doesn't have any methods just attributes 
